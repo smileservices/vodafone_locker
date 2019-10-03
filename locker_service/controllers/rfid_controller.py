c79592695d3a0b7d4dc1be4cf85864cfd866bade
vladimir.gorea <vladimir.gorea@vodafone.com>
2019-10-03 15:45:27 +0300

    implement scan method in rfid controller

--- locker_service/controllers/rfid_controller.py
+++ locker_service/controllers/rfid_controller.py
@@ -1,3 +1,5 @@
+import mercury
+
 class RfidController:
 	GPIO_PIN = 15
 
@@ -7,5 +9,11 @@ class RfidController:
 		scan multiple times and hold the tags in a set
 		return iterable
 		'''
-		# return {2: [tags, tags], 4: [tags, tags]}
-		pass
+		reader = mercury.Reader('tmr:///dev/ttyUSB0')
+		reader.set_region('EU3')
+		reader.set_read_plan([1], 'GEN2')
+		tags_set = {}
+		for _ in range(10)
+			for tag in reader.read():
+				tags_set.add(tag)
+		return tags_set
